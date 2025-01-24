e7ab34d99835b1bb26fa9addd548c2314ceb8faf
Ktbch <odufotekayode4@gmail.com>
2025-01-24 13:53:13 +0100

    nav-links corrections

--- src/pages/participant/Questions.tsx
+++ src/pages/participant/Questions.tsx
@@ -1,4 +1,7 @@
 
+
+
+
 export const Questions = () => {
   return (
     <div>
--- src/shared/participants/constants.ts
+++ src/shared/participants/constants.ts
@@ -1,5 +1,5 @@
 export const NAV_LINKS = {
-    startQuiz: '1/start-quiz',
-    questions: '1/questions',
-    answers: '1/answers',
+    startQuiz: '/1/start-quiz',
+    questions: '/1/questions/1',
+    answers: '/1/answers',
 }
